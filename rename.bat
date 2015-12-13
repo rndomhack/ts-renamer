b275a825239b372738cf2ab4e6b89c143ea8c17d
rndomhack <rndom.hack@gmail.com>
2015-12-13 18:43:45 +0900

    Update settings.json

---
+++ rename.bat
@@ -0,0 +1,28 @@
+@echo off
+cd /d %~dp0
+set args=
+
+:arg1
+  set arg=%~1
+:arg2
+  if exist "%arg%" goto arg3
+  shift /1
+  if "%~1" == "" goto end
+  set arg=%arg%Å@%~1
+  goto arg2
+:arg3
+  set args=%args% "%arg%"
+  shift /1
+  if not "%~1" == "" goto arg1
+
+::--------------------------------------------------
+title ts-renamer
+
+for %%i in (%args%) do (
+  echo.
+  echo ### "%%~i"
+  node index -i "%%~i"
+)
+
+:end
+pause
