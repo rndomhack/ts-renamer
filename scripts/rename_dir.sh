da6f5aaa4ad1e3966c694ad1158fba7e9be4ac1d
rndomhack <rndom.hack@gmail.com>
2016-01-18 22:58:15 +0900

    update sh

--- scripts/rename_dir.sh
+++ scripts/rename_dir.sh
@@ -2,12 +2,12 @@
 
 basedir=`dirname "$0"`
 
-while [ $# -gt 0 ]; do
+while [ "$#" -gt 0 ]; do
     echo -e "\n## \"$1\""
 
     find "$1" -name "*.ts" | while read file; do
         echo -e "\n### \"$file\""
-        node "$basedir/../cli.js" -i "$file" -d "\${title}" -f "\${title}([ 第\${count2}話])([ 「\${subTitle}」])([ (\${channelUserName})])"
+        node "$basedir/../cli.js" -i "$file" -d '${title}' -f '${title}([ 第${count2}話])([ 「${subTitle}」])([ (${channelUserName})])'
 
         if [ $? -eq 0 ]; then
             echo "OK: $file" >> "$basedir/../log/rename.log"
