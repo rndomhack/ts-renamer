eb425ae35ad94470d368d3ac279486d7ca086565
rndomhack <rndom.hack@gmail.com>
2016-02-04 14:44:13 +0900

    Add related macro with title

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -5,7 +5,7 @@ basedir=`dirname "$0"`
 while [ "$#" -gt 0 ]; do
     echo -e "\n### \"$1\""
 
-    node "$basedir/../cli.js" -i "$1" -d '${title}' -f '${title}([ 第${count2}話])([ 「${subTitle}」])([ (${channelUserName})])'
+    node "$basedir/../cli.js" -i "$1" -d '${firstStartYYYY}_Q${firstStartQuarter}/${title}' -f '${title}([ 第${count2}話])([ 「${subTitle}」])([ (${channelUserName})])'
 
     if [ "$?" -eq 0 ]; then
         echo "OK: $1" >> "$basedir/../log/rename.log"
--- scripts/rename_dir.sh
+++ scripts/rename_dir.sh
@@ -7,7 +7,7 @@ while [ "$#" -gt 0 ]; do
 
     find "$1" -name "*.ts" | while read file; do
         echo -e "\n### \"$file\""
-        node "$basedir/../cli.js" -i "$file" -d '${title}' -f '${title}([ 第${count2}話])([ 「${subTitle}」])([ (${channelUserName})])'
+        node "$basedir/../cli.js" -i "$file" -d '${firstStartYYYY}_Q${firstStartQuarter}/${title}' -f '${title}([ 第${count2}話])([ 「${subTitle}」])([ (${channelUserName})])'
 
         if [ "$?" -eq 0 ]; then
             echo "OK: $file" >> "$basedir/../log/rename.log"
