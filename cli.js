5565dea010988bc0a051a78021b1f6bc471ed1d4
rndomhack <rndom.hack@gmail.com>
2016-05-29 00:41:10 +0900

    Add 192 ts support

--- cli.js
+++ cli.js
@@ -8,6 +8,7 @@ cli.parse({
     parent: ["p", "Parent folder", "string", ""],
     dir: ["d", "Directory name", "string", ""],
     file: ["f", "File name", "string", ""],
+    packetSize: ["a", "Packet size", "number", 188],
     checkDrop: ["r", "Check drop", "boolean", false],
     checkDup: ["u", "Check duplication", "boolean", false],
     checkService: ["s", "Check service", "boolean", false]
@@ -20,7 +21,7 @@ cli.main((args, options) => {
         return;
     }
 
-    var renamer = new Renamer(options);
+    let renamer = new Renamer(options);
 
     renamer.execute().then(() => {
         console.log("Successful");
---
+++ scripts/rename_192.bat
@@ -0,0 +1,47 @@
+@echo off
+
+set args=
+
+:arg1
+    set arg=%~1
+:arg2
+    if exist "%arg%" goto arg3
+    shift /1
+    if "%~1" == "" goto end
+    set arg=%arg%　%~1
+    goto arg2
+:arg3
+    set args=%args% "%arg%"
+    shift /1
+    if not "%~1" == "" goto arg1
+
+title ts-renamer
+
+for %%i in (%args%) do (
+    setlocal
+    call :rename "%%~i"
+    endlocal
+)
+goto end
+
+:rename
+    echo.
+    echo ### "%~1"
+
+    node "%~dp0../cli.js" -i "%~1" -d "${firstStartYYYY}_Q${firstStartQuarter}/${title}" -f "${title}([ 第${count2}話])([ 「${subTitle}」])([ (${channelUserName})])" -a 192
+    call :log "%~1" %%errorlevel%%
+
+    goto :eof
+
+:log
+    if %2 == 0 (
+        echo OK: "%~1" >> "%~dp0../log/rename.log"
+    ) else (
+        echo NG: "%~1" >> "%~dp0../log/rename.log"
+    )
+
+    goto :eof
+
+:end
+    echo done
+    pause
