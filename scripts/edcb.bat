9f6ce8e161f0be07d534888459587689f7b86ade
rndomhack <rndom.hack@gmail.com>
2017-08-25 16:35:38 +0900

    v1.4.1

--- scripts/edcb.bat
+++ scripts/edcb.bat
@@ -1,5 +1,7 @@
 @echo off
 
+set src=
+
 set parent=
 set dir=${firstStartYYYY}_Q${firstStartQuarter}/${title}
 set file=${title}([ ëÊ${count2}òb])([ Åu${subTitle}Åv])([ (${channelUserName})])
@@ -11,13 +13,11 @@ set check_time=false
 set check_dup=false
 set check_drop=false
 
-set log_file=%~dp0..\log\rename.log
-set get_args=true
+set log_file=%src%\log\rename.log
+set get_args=false
 
-if exist "%~dp0..\bin\node.exe" (
-  "%~dp0..\bin\node.exe" "%~dp0..\dragdrop.js"
+if exist "%src%\bin\node.exe" (
+  "%src%\bin\node.exe" "%src%\dragdrop.js" "$FilePath$"
 ) else (
-  node "%~dp0..\dragdrop.js"
+  node "%src%\dragdrop.js" "$FilePath$"
 )
-
-pause
